c05f24fbe5715be85b5273988e082e3e59e2215e
Максим Кайряк <mkayryak3@yandex.ru>
2024-01-08 20:50:35 +0300

    На ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector('#card-template').content;
+const cardOnline = document.querySelector('.places__list');
+
+initialCards.forEach(function(element){
+
+const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+const garb = cardElement.querySelector('.card__delete-button');
+
+cardElement.querySelector('.card__image').src = element.link;
+cardElement.querySelector('.card__title').textContent = element.name;
+
+garb.addEventListener('click', function(){
+    cardElement.remove();
+})
+
+cardOnline.append(cardElement); 
+})
+
+
+
